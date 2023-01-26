bd48abc83813a55bbe2707f3fb0e6256bd541b46
jeffknowlesjr <jeffknowlesjr@gmail.com>
2023-01-26 17:53:58 -0500

    Functional component update

--- src/App.js
+++ src/App.js
@@ -1,66 +1,37 @@
-import { Component } from 'react'
+import { useState, useEffect } from 'react'
 import CardList from './components/card-list/card-list.component'
 import SearchBox from './components/search-box/search-box.component'
 
-class App extends Component {
-  constructor(props) {
-    super() // calls constructor method on the extnded component
+const App = () => {
+  const [searchField, setSearchField] = useState('') // [value, setValue]
+  const [dogs, setDogs] = useState([])
 
-    this.state = {
-      dogs: [],
-      searchField: ''
-    }
+  console.log('render')
 
-    console.log('constructor')
+  fetch('https://jsonplaceholder.typicode.com/users')
+    .then((res) => res.json())
+    .then((data) => setDogs(data))
+
+  const onSearchChange = (e) => {
+    const searchFieldString = e.target.value.toLocaleLowerCase()
+    setSearchField(searchFieldString)
   }
 
-  componentDidMount() {
-    console.log('componentDidMount App.js')
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then((res) => res.json())
-      .then((data) =>
-        this.setState(
-          () => {
-            return { dogs: data }
-          },
-          () => {
-            console.log(this.state)
-          }
-        )
-      )
-  }
+  const filteredDogs = dogs.filter((dog) => {
+    return dog.name.toLocaleLowerCase().includes(searchField)
+  })
 
-  onSearchChange = (e) => {
-    const searchField = e.target.value.toLocaleLowerCase()
-    console.log(`searchField: ${searchField}`)
-    this.setState(() => {
-      return { searchField }
-    })
-  }
-
-  render() {
-    console.log('render from App.js')
-
-    // Destructuring
-    const { dogs, searchField } = this.state
-    const { onSearchChange } = this
-
-    const filteredDogs = dogs.filter((dog) => {
-      return dog.name.toLocaleLowerCase().includes(searchField)
-    })
-
-    return (
-      <div className="App">
-        <h1 className="site-header">Dogs Rolodex</h1>
-        <SearchBox
-          onChangeHandler={onSearchChange}
-          placeholder={'Search Dogs'}
-          className={'dogs-search-box'}
-        />
-        <CardList dogs={filteredDogs} />
-      </div>
-    )
-  }
+  return (
+    <div className="App">
+      <h1 className="site-header">Dogs Rolodex</h1>
+      <SearchBox
+        onChangeHandler={onSearchChange}
+        placeholder={'Search Dogs'}
+        className={'dogs-search-box'}
+      />
+      <CardList dogs={filteredDogs} />
+    </div>
+  )
 }
 
 export default App
