c82c091aa5b867156f3e5e7772a25c869c510f00
jeffknowlesjr <jeffknowlesjr@gmail.com>
2023-01-26 18:56:52 -0500

    Converted remaining to function components

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,20 +1,15 @@
-import { Component } from 'react'
 import Card from '../card/card.component'
 import './card-list.styles.css'
 
-class CardList extends Component {
-  render() {
-    console.log('render from card list')
-    const { dogs } = this.props
-
-    return (
-      <div className="card-list">
-        {dogs.map((dog) => (
-          <Card key={dog.id} dog={dog} />
-        ))}
-      </div>
-    )
-  }
+const CardList = ({ dogs }) => {
+  console.log('render from CardList')
+  return (
+    <div className="card-list">
+      {dogs.map((dog) => (
+        <Card key={dog.id} dog={dog} />
+      ))}
+    </div>
+  )
 }
 
 export default CardList
